738939ccf34eddaec36af93665469a0db1ee9ae6
TheJordisc <jsolaceada@gmail.com>
2017-11-26 21:01:24 +0100

    -Testing background music (it works!) -Started Main Activity layout

--- app/src/main/java/net/xeill/elpuig/thinkitapp/MainActivity.java
+++ app/src/main/java/net/xeill/elpuig/thinkitapp/MainActivity.java
@@ -1,13 +1,31 @@
 package net.xeill.elpuig.thinkitapp;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        player = MediaPlayer.create(this,  R.raw.theme);
+        player.start();
+        player.setLooping(true); // Set looping
+        player.setVolume(100,100);
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        player.stop();
+        player.release();
     }
 }
