d6c42dc92bf413f45d139f2ceee50850b6adee55
TheJordisc <jsolaceada@gmail.com>
2017-12-03 18:34:18 +0100

    -Testing background music (it works!) -Started Main Activity layout

--- app/src/main/java/net/xeill/elpuig/thinkitapp/MainActivity.java
+++ app/src/main/java/net/xeill/elpuig/thinkitapp/MainActivity.java
@@ -1,31 +1,48 @@
 package net.xeill.elpuig.thinkitapp;
 
-import android.content.Intent;
+import android.media.AudioAttributes;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
-    MediaPlayer player;
+    MediaPlayer musicPlayer;
+    MediaController.MediaPlayerControl videoPlayer;
+    MediaController mediaController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        player = MediaPlayer.create(this,  R.raw.theme);
-        player.start();
-        player.setLooping(true); // Set looping
-        player.setVolume(100,100);
+        musicPlayer = MediaPlayer.create(this,  R.raw.theme);
+        musicPlayer.start();
+        musicPlayer.setLooping(true); // Set looping
+        musicPlayer.setVolume(100,100);
+
+        VideoView bgVideo = findViewById(R.id.bg_video);
+        bgVideo.setVideoURI(Uri.parse("android.resource://net.xeill.elpuig.thinkitapp/" + R.raw.background));
+        bgVideo.start();
+
+        bgVideo.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);
+            }
+        });
 
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        player.stop();
-        player.release();
+        musicPlayer.stop();
+        musicPlayer.release();
+        //videoPlayer.stop();
+        //videoPlayer.release();
     }
 }
