6163b1c6f92cd490fcb4f7872018e184003d76b5
Alejandro Berdún <33780414+aberdun01@users.noreply.github.com>
2017-12-26 13:17:19 +0100

    Add files via upload

--- app/src/main/java/net/xeill/elpuig/thinkitapp/LanguageActivity.java
+++ app/src/main/java/net/xeill/elpuig/thinkitapp/LanguageActivity.java
@@ -1,4 +1,10 @@
 package net.xeill.elpuig.thinkitapp;
 
+import android.os.Bundle;
+
 public class LanguageActivity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_language);
+    }
 }
