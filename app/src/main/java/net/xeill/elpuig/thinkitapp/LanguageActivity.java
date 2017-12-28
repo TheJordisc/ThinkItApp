e3735726e6f8c9727c8ba057cd80685c8e49f657
aberdun01 <33780414+aberdun01@users.noreply.github.com>
2017-12-28 19:29:07 +0100

    Added some code to LanguageActivity

--- app/src/main/java/net/xeill/elpuig/thinkitapp/LanguageActivity.java
+++ app/src/main/java/net/xeill/elpuig/thinkitapp/LanguageActivity.java
@@ -1,13 +1,57 @@
 package net.xeill.elpuig.thinkitapp;
 
+import android.content.Intent;
+import android.os.LocaleList;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class LanguageActivity extends AppCompatActivity {
-
+//TODO: play sound + setLocale
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_language);
+        final ImageButton cat_flag = findViewById(R.id.catButton1);
+        cat_flag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Catalan");
+                menuIntent();
+            }
+        });
+
+        final ImageButton es_flag = findViewById(R.id.esButton2);
+        es_flag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Español");
+                menuIntent();
+            }
+        });
+
+        final ImageButton it_flag = findViewById(R.id.itButton3);
+        it_flag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Italiano");
+                menuIntent();
+            }
+        });
+
+        final ImageButton uk_flag = findViewById(R.id.ukButton4);
+        uk_flag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("English");
+                menuIntent();
+            }
+        });
+    }
+        public void menuIntent(){
+            Intent menuIntent = new Intent(LanguageActivity.this,MainActivity.class);
+            startActivity(menuIntent);
     }
 }
