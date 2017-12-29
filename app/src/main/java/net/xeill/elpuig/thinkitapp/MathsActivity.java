2f0fd05b9aea2c02a367e43b212430f737ca8ac8
TheJordisc <jsolaceada@gmail.com>
2017-12-29 17:42:15 +0100

    -Changed MathsActivity to almost-final code structure for scalability's sake.

--- app/src/main/java/net/xeill/elpuig/thinkitapp/MathsActivity.java
+++ app/src/main/java/net/xeill/elpuig/thinkitapp/MathsActivity.java
@@ -49,6 +49,134 @@ public class MathsActivity extends AppCompatActivity {
         TextView correctText = findViewById(R.id.correctAnswers);
         correctText.setText(correctAnswers+"");
 
+        //VERSIÓN ESCALABLE
+
+        Operation op1;
+
+        do {
+            //Operación vacía
+            op1 = new Operation();
+
+            //Seleccionar operación
+            op1.setOpType(Operation.OpType.values()[(int) (Math.random() * 4)]);
+
+            //Calcular operando 1
+            int range = (100 - 1) + 1;
+            op1.setOp1((int)(Math.random() * range) + 1);
+
+            //Calcular operando 2
+            if(op1.getOpTypeStr().equals("/") || op1.getOpTypeStr().equals("*")) {
+                range = (10 - 1) + 1;
+            }
+
+            int newOp2 = (int)(Math.random() * range) + 1;
+            while (op1.getOpTypeStr().equals("/") && op1.getOp1()%newOp2 !=0) {
+                newOp2 = (int)(Math.random() * range) + 1;
+            }
+            op1.setOp2(newOp2);
+        } while ((op1.getOpTypeStr().equals("/") && op1.getOp1()/op1.getOp2() < 0) ||(op1.getOpTypeStr().equals("-") && op1.getOp1()-op1.getOp2() < 0));
+
+        //Guardar resultado
+        op1.calculate();
+
+        //Calcular campo escondido
+        int hiddenField = (int)(Math.random() * ((2 - 0) + 1)) + 0;
+
+        TextView op1Op1TV = findViewById(R.id.oper1_op1);
+        TextView op1Op2TV = findViewById(R.id.oper1_op2);
+        TextView op1ResTV = findViewById(R.id.oper1_res);
+
+        //Poner símbolo de operación
+        TextView op1OpType = findViewById(R.id.oper1_opType);
+        op1OpType.setText(op1.getOpTypeStr());
+
+        //Poner campos según toque
+        switch (hiddenField) {
+            case 0:
+                op1Op1TV.setText("?");
+                op1Op2TV.setText(op1.getOp2()+"");
+                op1ResTV.setText(op1.getRes()+"");
+                break;
+            case 1:
+                op1Op1TV.setText(op1.getOp1()+"");
+                op1Op2TV.setText("?");
+                op1ResTV.setText(op1.getRes()+"");
+                break;
+            case 2:
+                op1Op1TV.setText(op1.getOp1()+"");
+                op1Op2TV.setText(op1.getOp2()+"");
+                op1ResTV.setText("?");
+                break;
+        }
+
+        //Rellenar teclado
+        List<Button> answerButtons = new ArrayList<>();
+        answerButtons.add((Button) findViewById(R.id.answer1));
+        answerButtons.add((Button) findViewById(R.id.answer2));
+        answerButtons.add((Button) findViewById(R.id.answer3));
+        answerButtons.add((Button) findViewById(R.id.answer4));
+        answerButtons.add((Button) findViewById(R.id.answer5));
+        answerButtons.add((Button) findViewById(R.id.answer6));
+        answerButtons.add((Button) findViewById(R.id.answer7));
+        answerButtons.add((Button) findViewById(R.id.answer8));
+
+        //Clear buttons
+        for (Button b : answerButtons) {
+            b.setText("");
+        }
+
+        int correctButtonIndex = (int) (Math.random() * 8);
+
+        switch (hiddenField) {
+            case 0:
+                answerButtons.get(correctButtonIndex).setText(op1.getOp1() + "");
+                break;
+            case 1:
+                answerButtons.get(correctButtonIndex).setText(op1.getOp2() + "");
+                break;
+            case 2:
+                answerButtons.get(correctButtonIndex).setText(op1.getRes() + "");
+                break;
+        }
+
+        //Margen +-10
+        for (int i = 0; i < answerButtons.size(); i++) {
+            if (answerButtons.get(i).getText()=="") {
+                int answerRange=0;
+                switch (hiddenField) {
+                    case 0:
+                        answerRange = ((op1.getOp1()+10) - (op1.getOp1()-10)) + 1;
+                        break;
+                    case 1:
+                        answerRange = ((op1.getOp2()+10) - (op1.getOp2()-10)) + 1;
+                        break;
+                    case 2:
+                        answerRange = ((op1.getRes()+10) - (op1.getRes()-10)) + 1;
+                        break;
+                }
+
+                answerButtons.get(i).setText(((int)(Math.random() * answerRange) + op1.getRes()-10) + "");
+                //Pa que no se repita
+                //TODO: Se repiten a veces con las divisiones
+                while (answerButtons.get(i).getText().equals(op1.getRes())) {
+                    answerButtons.get(i).setText(((int)(Math.random() * answerRange) + op1.getRes()-10) + "");
+                }
+            }
+        }
+
+        answerButtons.get(correctButtonIndex).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                correctAnswers++;
+                //TODO: El listener se queda incluso en otras iteraciones. La primera vez va bien,
+                // luego se van acumulando listeners y al final todas son correctas.
+                loadOperation();
+            }
+        });
+
+
+        //VERSIÓN INICIAL
+        /*
         //Probando números fáciles
         int range = (100 - 1) + 1;
         Operation op1 = new Operation((int)(Math.random() * range) + 1,(int)(Math.random() * range) + 1);
@@ -108,6 +236,8 @@ public class MathsActivity extends AppCompatActivity {
                 loadOperation();
             }
         });
+
+        */
     }
 
     @Override
