7d68f3378897dfa18d6c086cc55be9b579c532ec
aberdun01 <alejandrokad@gmail.com>
2018-01-02 20:38:42 +0100

    Created ExpandableListAdapter class. Created list_group and list_item files. Added collapse/expand expansion panels.

--- app/src/main/java/net/xeill/elpuig/thinkitapp/HelpActivity.java
+++ app/src/main/java/net/xeill/elpuig/thinkitapp/HelpActivity.java
@@ -1,38 +1,47 @@
 package net.xeill.elpuig.thinkitapp;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
+import android.widget.ExpandableListView;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class HelpActivity extends AppCompatActivity {
+
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
 
-        final ImageButton logoButton = findViewById(R.id.logoButton);
-        logoButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Uri uri = Uri.parse("https://github.com/TheJordisc/ThinkItApp");
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
-            }
-        });
+        expListView = (ExpandableListView) findViewById(R.id.lvExp);
 
-        final ImageButton switchButton = findViewById(R.id.switchLogo);
-        switchButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Uri uri = Uri.parse("https://elpuig.xeill.net/el-centre/erasmus/switch-project");
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
-            }
-        });
+        prepareListData();
+
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+
+        expListView.setAdapter(listAdapter);
     }
 
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        listDataHeader.add("Créditos");
+        listDataHeader.add("Menú principal");
+
+        List<String> creditos = new ArrayList<String>();
+        creditos.add("By Jordi & alejandro");
+
+        List<String> menuHelp = new ArrayList<String>();
+        menuHelp.add("Ayuda del menú principal:");
+
+        listDataChild.put(listDataHeader.get(0), creditos);
+        listDataChild.put(listDataHeader.get(1), menuHelp);
+    }
 }
--- app/src/main/java/net/xeill/elpuig/thinkitapp/LanguageActivity.java
+++ app/src/main/java/net/xeill/elpuig/thinkitapp/LanguageActivity.java
@@ -5,7 +5,9 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 
 public class LanguageActivity extends AppCompatActivity {
     MediaPlayer playSoundPlayer;
