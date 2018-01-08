74c48650e278fb33c473f6d4e481de8c0aca4c76
aberdun01 <33780414+aberdun01@users.noreply.github.com>
2018-01-08 22:42:34 +0100

    Added logos into HelpActivity

--- app/src/main/java/net/xeill/elpuig/thinkitapp/HelpActivity.java
+++ app/src/main/java/net/xeill/elpuig/thinkitapp/HelpActivity.java
@@ -1,8 +1,13 @@
 package net.xeill.elpuig.thinkitapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ExpandableListView;
+import android.widget.ImageButton;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -18,6 +23,27 @@ public class HelpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        final ImageButton logoButton = findViewById(R.id.logoButton);
+        logoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://github.com/TheJordisc/ThinkItApp");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        final ImageButton switchButton = findViewById(R.id.switchLogo);
+        switchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://elpuig.xeill.net/el-centre/erasmus/switch-project");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
 
         expListView = (ExpandableListView) findViewById(R.id.lvExp);
 
